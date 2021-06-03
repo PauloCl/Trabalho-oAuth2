b8ae2fc13bb52c95ff72f938fa485fd030e26a79
Paulo C <paulocl908@gmail.com>
2021-06-03 20:37:46 -0300

    correção pasta

--- source/Config.php
+++ source/Config.php
@@ -3,6 +3,6 @@
 define("GOOGLE", [
     'clientId' => '219515872867-c4d7l14qbq2u9n86v3cs7ateec4b91d8.apps.googleusercontent.com',
     'clientSecret' => 'CEcx3H92GrPXXmVkVuk9x17M',
-    'redirectUri' => 'http://localhost/oAuth2/index.php',
+    'redirectUri' => 'http://localhost/Trabalho-oAuth2/index.php',
 
 ]);
